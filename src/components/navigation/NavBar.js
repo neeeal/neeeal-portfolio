c69cb1d49a511d7964dd835314310c06e314be20
neeeal <matira.nealbartonjames@gmail.com>
2024-08-11 22:07:20 +0800

    feat: apply bg and gradient

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -6,12 +6,12 @@ export default function NavBar() {
   return (
     <div className={styles.NavBar}>
       <div className={styles.HeaderContainer}>
-      <h1 className={styles.Header}><a onClick={()=> scroll.scrollToTop()}>NavBar</a></h1>
+      <h1 className={styles.Header}><span onClick={()=> scroll.scrollToTop()}>NavBar</span></h1>
       </div>
       <div className={styles.Container}>
         <Link to='skills' smooth={true} duration={500} className={styles.Link}>Skills</Link>
-        <Link to='projects' smooth={true} duration={500} className={styles.Projects}><Icon icon='carbon:gears' style={{ fontSize: '24px', color: '#E84949' }} /><span>Projects</span></Link>
-        <Link to='contactMe' smooth={true} duration={500} className={styles.Link}>Contact Me</Link>
+        <Link to='projects' smooth={true} duration={700} className={styles.Projects}><Icon icon='carbon:gears' style={{ fontSize: '24px', color: '#E84949' }} /><span>Projects</span></Link>
+        <Link to='contactMe' smooth={true} duration={900} className={styles.Link}>Contact Me</Link>
       </div>
     </div>
   )
