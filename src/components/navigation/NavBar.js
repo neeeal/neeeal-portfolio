bc360508b5ad0b96c131c29ccd0ffeba273c279e
Niil <91337648+neeeal@users.noreply.github.com>
2024-08-12 12:49:07 +0800

    feat: fixed navigation and home css

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import { Icon } from '@iconify/react';
 import styles from './NavBar.module.css';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
 export default function NavBar() {
+  const [ isHovered, setIsHovered ] = useState(false);
   return (
     <div className={styles.NavBar}>
       <div className={styles.HeaderContainer}>
-      <h1 className={styles.Header}><span onClick={()=> scroll.scrollToTop()}>NavBar</span></h1>
+        <h1 className={styles.Header}><span onClick={()=> scroll.scrollToTop()}>neeeal</span></h1>
       </div>
       <div className={styles.Container}>
         <Link to='skills' smooth={true} duration={500} className={styles.Link}>Skills</Link>
-        <Link to='projects' smooth={true} duration={700} className={styles.Projects}><Icon icon='carbon:gears' style={{ fontSize: '24px', color: '#E84949' }} /><span>Projects</span></Link>
+        <Link to='projects' smooth={true} duration={700} className={styles.Projects} 
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}>
+            <Icon icon='carbon:gears' 
+              style={{ 
+                fontSize: '24px', 
+                color: isHovered ? 'white' : '#E84949' }}/>
+          <span>Projects</span>
+          </Link>
         <Link to='contactMe' smooth={true} duration={900} className={styles.Link}>Contact Me</Link>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import styles from '../css/Home.module.css';
+import { Link } from 'react-scroll';
 
 export default function Home() {
   return (
@@ -6,12 +7,13 @@ export default function Home() {
       <div className={styles.Section}>
         <div className={styles.Container}>
           <div className={styles.Description}>
-            <h1 >Hello, I'm Neal</h1>
-            <p>A software engineer dedicated to making the world a better place one line of code at a time.</p>
+            <h1>Hello, I'm Neal</h1>
+            <p>A <b>Software Engineer</b> dedicated to making the world a better place one line of code at a time.</p>
+            <Link to="contactMe" smooth={true} duration={500} className={styles.Button}>Contact Me</Link>
           </div>
 
           <div className={styles.ImageContainer}>
-            <img src="https://via.placeholder.com/700" alt="Placeholder" />
+            <img src={require('../assets/images/91337648.png')} className={styles.HeroImage} alt="Placeholder"/>
           </div>
         </div>
       </div>
@@ -20,19 +22,25 @@ export default function Home() {
 
       <div className={styles.Section}>
         <div className={styles.Container}>
-          <div className={styles.Description}>
-            <p>I have a vast experience in the following web technologies:</p>
+          <div className={styles.SkillsDescription}>
+            <p>I have a vast experience in the following technologies:</p>
             <div className={styles.ImageContainer}>
-            <img src="https://via.placeholder.com/300" alt="Placeholder" className={styles.Image}/>
-            <img src="https://via.placeholder.com/300" alt="Placeholder" className={styles.Image}/>
-            <img src="https://via.placeholder.com/300" alt="Placeholder" className={styles.Image}/>
-            <img src="https://via.placeholder.com/300" alt="Placeholder" className={styles.Image}/>
-            <img src="https://via.placeholder.com/300" alt="Placeholder" className={styles.Image}/>
-            <img src="https://via.placeholder.com/300" alt="Placeholder" className={styles.Image}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" className={styles.SkillImage}/>
             </div>
           </div>
           <div className={styles.ImageContainer}>
-            <img src="https://via.placeholder.com/600" alt="Placeholder" className={styles.Image}/>
+            <img src="https://via.placeholder.com/500" alt="Placeholder" className={styles.Image}/>
           </div>
         </div>
       </div>
